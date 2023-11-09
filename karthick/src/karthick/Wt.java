bed15c5a329875dce1c9f5963a3baef2f4a08530
srike <srike@kuttykeerthi>
2023-11-09 18:52:28 +0530

    hjfhdth

---
+++ karthick/src/karthick/Wt.java
@@ -0,0 +1,16 @@
+package karthick;
+
+public class Wt {
+	
+	private void je() {
+		 System.out.println("keerthi is a good boy");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Wt t=new Wt();
+		t.je();
+	}
+
+}
